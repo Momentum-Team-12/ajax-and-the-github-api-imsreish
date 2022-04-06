43a62597431419a48040c4b4995062ec843379ab
Stephen Reish <spreish@ncsu.edu>
2022-04-06 18:51:28 -0400

    Added primary data. No styling & have yet to figure out how to iterate thru repos

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 console.log("JS is connected");
 
 let profileDiv = document.querySelector("#profile");
+let repoDiv = document.querySelector("#repos");
 
 fetch("https://api.github.com/users/imsreish", {
   method: "GET",
@@ -10,6 +11,7 @@ fetch("https://api.github.com/users/imsreish", {
     return response.json();
   })
   .then(function (data) {
+    console.log(data);
     console.log(data.avatar_url);
     console.log(data.name);
     console.log("Location:", data.location);
@@ -17,10 +19,42 @@ fetch("https://api.github.com/users/imsreish", {
     console.log("GitHub username:", data.login);
     console.log("GitHub Repos:", data.public_repos);
 
-    let nameDiv = document.createElement("p");
-    nameDiv.classList.add("name");
-    nameDiv.innerText = data.name;
-    profileDiv.appendChild(nameDiv);
+    let aviDisplay = document.createElement("img");
+    aviDisplay.classList.add("avi");
+    aviDisplay.src = data.avatar_url;
+    profileDiv.appendChild(aviDisplay);
+
+    let nameDisplay = document.createElement("h1");
+    nameDisplay.classList.add("name");
+    nameDisplay.innerText = data.name;
+    profileDiv.appendChild(nameDisplay);
+
+    // let isHireable = document.createElement("h4");
+    // isHireable.classList.add("hireable");
+    // isHireable.innerText = data.hireable;
+    // profileDiv.appendChild(isHireable);
+
+    let usernameDisplay = document.createElement("a");
+    usernameDisplay.classList.add("username");
+    usernameDisplay.innerText = data.login;
+    usernameDisplay.href = "https://github.com/" + data.login;
+    profileDiv.appendChild(usernameDisplay);
+
+    let locationDisplay = document.createElement("h2");
+    locationDisplay.classList.add("location");
+    locationDisplay.innerText = data.location;
+    profileDiv.appendChild(locationDisplay);
+
+    let twitterDisplay = document.createElement("a");
+    twitterDisplay.classList.add("twitter");
+    twitterDisplay.innerText = `@${data.twitter_username}`;
+    twitterDisplay.href = "https://twitter.com/" + data.twitter_username;
+    profileDiv.appendChild(twitterDisplay);
+
+    let bioDisplay = document.createElement("p");
+    bioDisplay.classList.add("bio");
+    bioDisplay.innerText = data.bio;
+    profileDiv.appendChild(bioDisplay);
   });
 
 fetch("https://api.github.com/users/imsreish/repos", {
@@ -31,5 +65,12 @@ fetch("https://api.github.com/users/imsreish/repos", {
     return response.json();
   })
   .then(function (data) {
-    console.log("The repos contain:", data);
+    console.log(data);
+    const publicRepos = data.name;
+    for (let publicRepo of publicRepos) {
+      let oneRepo = document.createElement("div");
+      oneRepo.classList.add("repo");
+      oneRepo.innerText = publicRepos;
+      repoDiv.appendChild(oneRepo);
+    }
   });
